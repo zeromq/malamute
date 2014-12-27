cf3390248381077a8b02e3e9004c80b03c995f56
Pieter Hintjens <ph@imatix.com>
2014-12-27 17:37:11 +0100

    Problem: mshell doesn't use authentication
    Solution: use PLAIN authentication, and mshell/mshell credentials.

--- src/mshell.c
+++ src/mshell.c
@@ -22,7 +22,7 @@ int main (int argc, char *argv [])
         printf ("syntax: mshell stream type [ body ]\n");
         return 0;
     }
-    mlm_client_t *client = mlm_client_new ("ipc://@/malamute", 1000, "mshell");
+    mlm_client_t *client = mlm_client_new ("ipc://@/malamute", 1000, "mshell/mshell");
     if (!client) {
         zsys_error ("mshell: server not reachable at ipc://@/malamute");
         return 0;
