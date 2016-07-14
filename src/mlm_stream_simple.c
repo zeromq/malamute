aa39cbb85e4abc942b6a95f082f0069b6825d031
Chernikava, Alena <AlenaChernikava@Eaton.com>
2016-07-14 15:47:29 +0200

    Problem: s_self_new & Ko ambiguos function names Solution: rename them to s_stream_new &Ko

--- src/mlm_stream_simple.c
+++ src/mlm_stream_simple.c
@@ -64,7 +64,7 @@ s_selector_new (void *client, const char *pattern)
 
 
 //  --------------------------------------------------------------------------
-//  The self_t structure holds the state for one actor instance
+//  The stream_t structure holds the state for one actor instance
 
 typedef struct {
     zsock_t *cmdpipe;           //  Actor command pipe
@@ -73,12 +73,12 @@ typedef struct {
     bool terminated;            //  Did caller ask us to quit?
     bool verbose;               //  Verbose logging enabled?
     zlistx_t *selectors;        //  List of selectors we hold
-} self_t;
+} stream_t;
 
-static self_t *
-s_self_new (zsock_t *cmdpipe, zsock_t *msgpipe)
+static stream_t *
+s_stream_new (zsock_t *cmdpipe, zsock_t *msgpipe)
 {
-    self_t *self = (self_t *) zmalloc (sizeof (self_t));
+    stream_t *self = (stream_t *) zmalloc (sizeof (stream_t));
     self->cmdpipe = cmdpipe;
     self->msgpipe = msgpipe;
     self->poller = zpoller_new (self->cmdpipe, self->msgpipe, NULL);
@@ -88,11 +88,11 @@ s_self_new (zsock_t *cmdpipe, zsock_t *msgpipe)
 }
 
 static void
-s_self_destroy (self_t **self_p)
+s_stream_destroy (stream_t **self_p)
 {
     assert (self_p);
     if (*self_p) {
-        self_t *self = *self_p;
+        stream_t *self = *self_p;
         zpoller_destroy (&self->poller);
         zlistx_destroy (&self->selectors);
         zsock_destroy (&self->msgpipe);
@@ -102,7 +102,7 @@ s_self_destroy (self_t **self_p)
 }
 
 static void
-s_stream_compile (self_t *self, void *client, const char *pattern)
+s_stream_compile (stream_t *self, void *client, const char *pattern)
 {
     selector_t *selector = (selector_t *) zlistx_first (self->selectors);
     while (selector) {
@@ -126,7 +126,7 @@ s_stream_compile (self_t *self, void *client, const char *pattern)
 }
 
 static void
-s_stream_cancel (self_t *self, void *client)
+s_stream_cancel (stream_t *self, void *client)
 {
     selector_t *selector = (selector_t *) zlistx_first (self->selectors);
     while (selector) {
@@ -142,7 +142,7 @@ s_stream_cancel (self_t *self, void *client)
 //  Here we implement the stream API
 
 static int
-s_self_handle_command (self_t *self)
+s_stream_handle_command (stream_t *self)
 {
     char *method = zstr_recv (self->cmdpipe);
     if (!method)
@@ -182,7 +182,7 @@ s_self_handle_command (self_t *self)
 
 
 static int
-s_self_handle_message (self_t *self)
+s_stream_handle_message (stream_t *self)
 {
     void *sender;
     mlm_msg_t *msg;
@@ -211,22 +211,22 @@ s_self_handle_message (self_t *self)
 void
 mlm_stream_simple (zsock_t *pipe, void *args)
 {
-    self_t *self = s_self_new (pipe, (zsock_t *) args);
+    stream_t *self = s_stream_new (pipe, (zsock_t *) args);
     //  Signal successful initialization
     zsock_signal (pipe, 0);
 
     while (!self->terminated) {
         zsock_t *which = (zsock_t *) zpoller_wait (self->poller, -1);
         if (which == self->cmdpipe)
-            s_self_handle_command (self);
+            s_stream_handle_command (self);
         else
         if (which == self->msgpipe)
-            s_self_handle_message (self);
+            s_stream_handle_message (self);
         else
         if (zpoller_terminated (self->poller))
             break;          //  Interrupted
     }
-    s_self_destroy (&self);
+    s_stream_destroy (&self);
 }
 
 
